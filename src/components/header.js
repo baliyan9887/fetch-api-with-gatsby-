f22912c1690e7be8a4a17b1cc3238c29555338e7
baliyan9887 <31700398+baliyan9887@users.noreply.github.com>
2020-01-03 20:41:53 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#FFCC00`,
       marginBottom: `1.45rem`,
     }}
   >
