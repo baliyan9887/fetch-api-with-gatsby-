56a1b712b832b5179fb95f8cdbc2b7e29fa1662d
baliyan9887 <31700398+baliyan9887@users.noreply.github.com>
2020-01-03 20:52:36 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Bat Labs`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Marketing from the outer space`,
+    author: `@batlabs`,
   },
   proxy: {
     prefix: "/api",
